843a34824d8ee12f9449fcd5d1dd7d33421f1d17
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-01-09 15:16:39 +0700

    Update http_client.go

--- ioc/http_client.go
+++ ioc/http_client.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 const (
@@ -18,11 +19,17 @@ var (
 )
 
 func (c HTTPClient) getVirustotal(api string) ([]byte, error) {
+	/*proxyURL, _ := url.Parse("http://127.0.0.1:3131")
+	http.DefaultTransport = &http.Transport{
+		Proxy: http.ProxyURL(proxyURL),
+	}*/
 	req, _ := http.NewRequest("GET", api, nil)
 	req.Header.Set("X-Apikey", "7d42532bd1dea1e55f7a8e99cdee23d9b26c386a6485d6dcb4106b9d055f9277")
+	
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
+		fmt.Println(err)
 		return nil, err
 	}
 	c.info(fmt.Sprintf("GET %s -> %d", api, resp.StatusCode))
@@ -36,6 +43,10 @@ func (c HTTPClient) getVirustotal(api string) ([]byte, error) {
 }
 
 func (c HTTPClient) getOtx(api string) ([]byte, error) {
+	/*proxyURL, _ := url.Parse("http://127.0.0.1:3131")
+	http.DefaultTransport = &http.Transport{
+		Proxy: http.ProxyURL(proxyURL),
+	}*/
 	req, _ := http.NewRequest("GET", api, nil)
 	req.Header.Set("X-OTX-API-KEY", "779cc51038ddb07c5f6abe0832fed858a6039b9e8cdb167d3191938c1391dbba")
 	client := &http.Client{}
