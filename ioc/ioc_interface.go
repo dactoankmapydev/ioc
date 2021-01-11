2deafae05e5c83809ff656baa860536309ec8499
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-01-11 14:32:35 +0700

    Update ioc_interface.go

--- ioc/ioc_interface.go
+++ ioc/ioc_interface.go
@@ -4,16 +4,30 @@ package ioc
 type IocProvider interface {
 	//GetHuntingNotificationFiles(limit, cursor, filter string) (IocInfo, error)
 	//GetPulsesSubscribed(limit, page, modifiedSince string) (IocInfo, error)
-	Get(limit string) (IocInfo, error)
+	GetHuntingNotificationFiles(limit string) ([]VrttInfo, error)
+	//GetPulsesSubscribed(limit string) ([]IocInfo, error)
 }
 
-type IocInfo struct {
+type VrttInfo struct {
 	Name string
 	Sha256 string
 	Sha1 string
 	Md5 string
-	Tags []string
 	FirstSubmit string
 	NotificationDate string
 	FileType string
+	Tags []string
+	LastAnalysisResults []string
+}
+
+type OtxInfo struct {
+	PulseId string
+	PulseName string
+	IndicatorsId string
+	IndicatorName string
+	IndicatorType string
+	Created string
+	Modified string
+	NextPage string
+	Tags []string
 }
