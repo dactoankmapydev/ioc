465d55e5f036234ea3f63a0ff3e33b4ca5a021e9
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-01-11 18:30:55 +0700

    Update ioc_interface.go

--- ioc/ioc_interface.go
+++ ioc/ioc_interface.go
@@ -5,7 +5,6 @@ type IocProvider interface {
 	//GetHuntingNotificationFiles(limit, cursor, filter string) (IocInfo, error)
 	//GetPulsesSubscribed(limit, page, modifiedSince string) (IocInfo, error)
 	GetHuntingNotificationFiles(limit string) ([]VrttInfo, error)
-	//GetPulsesSubscribed(limit string) ([]IocInfo, error)
 }
 
 type VrttInfo struct {
@@ -13,14 +12,14 @@ type VrttInfo struct {
 	Sha256 string
 	Sha1 string
 	Md5 string
-	FirstSubmit string
-	NotificationDate string
+	FirstSubmit int
+	NotificationDate int
 	FileType string
 	Tags []string
 	LastAnalysisResults []string
 }
 
-type OtxInfo struct {
+/*type OtxInfo struct {
 	PulseId string
 	PulseName string
 	IndicatorsId string
@@ -30,4 +29,4 @@ type OtxInfo struct {
 	Modified string
 	NextPage string
 	Tags []string
-}
+}*/
