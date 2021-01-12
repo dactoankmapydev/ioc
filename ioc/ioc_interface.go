cb819e5fa198ef0e1070d85200769a630831046c
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-01-12 17:38:40 +0700

    Update ioc_interface.go

--- ioc/ioc_interface.go
+++ ioc/ioc_interface.go
@@ -4,5 +4,19 @@ import "ioc-provider/model"
 
 // IocProvider là interface để service implement các hàm
 type IocProvider interface {
-	GetHuntingNotificationFiles() ([]model.VrttInfo, error)
+	GetHuntingNotificationFiles() ([]VrttInfo, error)
+}
+
+type VrttInfo struct {
+	Name string `json:"names"`
+	Sha256 string `json:"sha256"`
+	Sha1 string `json:"sha1"`
+	Md5 string `json:"md5"`
+	FirstSubmit int `json:"first_submit"`
+	NotificationDate int `json:"notification_date"`
+	FileType string `json:"file_type"`
+	Tags []string `json:"tags"`
+	EnginesDetected []string `json:"engines_detected"`
+	Detected int `json:"detected"`
+	Point int `json:"point"`
 }
