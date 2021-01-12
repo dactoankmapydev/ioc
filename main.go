ae15e8268bc1f247a75da518fe798d1f1bdaec4f
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-01-12 08:12:35 +0700

    x

--- main.go
+++ main.go
@@ -94,7 +94,6 @@ func main()  {
 				LastAnalysisResults: value.LastAnalysisResults,
 			})
 		}
-
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(results)
 	}).Methods("GET")
