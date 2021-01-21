673a6d9eeb535e29ebf48753ed5c6db3dfe2ece5
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-01-21 18:53:08 +0700

    Update main.go

--- main.go
+++ main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/joho/godotenv"
-	ioc "ioc-provider/ioc"
+	"ioc-provider/crawler"
+	ioc "ioc-provider/repository"
 	"log"
 	"os"
 )
+
 // Danh sách các Provider
-type ProviderList []ioc.IocProvider
+type ProviderList []ioc.IocRepo
 
 func init() {
 	if err := godotenv.Load(".env"); err != nil {
@@ -17,21 +19,29 @@ func init() {
 
 func (list ProviderList) info() {
 	for _, p := range list {
-		func(provider ioc.IocProvider) {
-			provider.GetHuntingNotificationFiles()
+		func(provider ioc.IocRepo) {
+			provider.LiveHunting()
+			//provider.Subscribed()
 		}(p)
 	}
 }
 
 func main()  {
-	// Tạo provider để gọi api virustotal.com
-	virustotal := ioc.VirustotalProvider{
+
+	// Tạo provider để gọi api virustotal
+	virustotal := crawler.VirustotalProvider{
 		APIKey: os.Getenv("VIRUSTOTAL_API_KEY"),
 	}
+
+	// Tạo provider để gọi api otx
+	/*otx := crawler.OtxProvider{
+		APIKey: os.Getenv("OTX_API_KEY"),
+	}*/
 
 	// Danh sách chứa các service
 	iocList := ProviderList{
 		virustotal,
+		//otx,
 	}
 	iocList.info()
 }
